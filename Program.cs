e28531c8ae6e3dd73c53234a1406f912f93073dd
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-20 23:06:14 +0300

    finish task 8

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("введите число ");
+
+ int a = Convert.ToInt32(Console.ReadLine());
+       int res = 1;
+       while (res < a)
+       {
+        Console.WriteLine(res+1);
+        res+= 2;
+       }
+    
